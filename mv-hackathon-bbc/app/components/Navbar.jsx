0ee7f3dbf10086232d9dbf87bd2c9a809ff07037
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2024-10-21 14:33:35 +0800

    Add navbar

---
+++ mv-hackathon-bbc/app/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">TBD</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link href="/events/">Events</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}

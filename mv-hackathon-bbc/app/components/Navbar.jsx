b20746ead47e6e97ece310aeaab585b841129408
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2024-10-21 19:30:26 +0800

    Update navbar

--- mv-hackathon-bbc/app/components/Navbar.jsx
+++ mv-hackathon-bbc/app/components/Navbar.jsx
@@ -4,15 +4,28 @@ import Link from 'next/link';
 export default function Navbar() {
   return (
     <nav className="navbar bg-base-100 custom-container">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">TBD</a>
+      <div className="navbar-start">
+        <span className="text-xl font-bold">UPV Ganaps</span>
       </div>
-      <div className="flex-none">
+      <div className="navbar-center">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <Link href="/events/">Events</Link>
+            <Link href="/">Events</Link>
+          </li>
+          <li>
+            <Link href="/events/create">Create</Link>
           </li>
         </ul>
+      </div>
+      <div className="navbar-end">
+        <div className="flex gap-2">
+          <Link href="/login/" className="btn btn-primary btn-outline btn-sm">
+            Sign in
+          </Link>
+          <Link href="/register/" className="btn btn-primary btn-sm">
+            Sign up
+          </Link>
+        </div>
       </div>
     </nav>
   );
